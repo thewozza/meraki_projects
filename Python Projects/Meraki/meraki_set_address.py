3af0cdb7c409f3d6e2167d200c15ef66b7348f4b
Paul <Paul@DESKTOP-1TUAUSK>
2019-10-31 12:04:01 -0700

    updated code

--- Python Projects/Meraki/meraki_set_address.py
+++ Python Projects/Meraki/meraki_set_address.py
@@ -2,12 +2,29 @@ import requests
 import json
 import sys
 
-networkid = sys.argv[1]
-address = sys.argv[2]
+APIkey = sys.argv[1]
+networkid = sys.argv[2]
+address = sys.argv[3]
+
+url = "https://api.meraki.com/api/v0/networks/"+networkid+"/devices"
+
+headers = {
+    'Accept': "*/*",
+    'X-Cisco-Meraki-API-Key': APIkey,
+    'User-Agent': "PostmanRuntime/7.19.0",
+    'Cache-Control': "no-cache",
+    'Postman-Token': "cc840ef2-8e18-46c0-a4c5-c4330a225f2d,9a2f97c9-2094-4989-83c0-f07adc99ae11",
+    'Accept-Encoding': "gzip, deflate",
+    'Referer': "https://api.meraki.com/api/v0/networks/"+networkid+"/devices",
+    'Connection': "keep-alive",
+    'cache-control': "no-cache"
+    }
+
+response = requests.request("GET", url, headers=headers).text
 
 for json_data in json.loads(response):
     if json_data["address"] == "":
-        url = "https://api.meraki.com/api/v0/networks/"+networkid+"/devices/" + json_data["serial"]
+        url = "https://api.meraki.com/api/v0/networks/"+networkid+"/devices/"+json_data["serial"]
 
         querystring = {"address":address}
         
@@ -16,7 +33,7 @@ for json_data in json.loads(response):
         headers = {
             'Accept': "*/*",
             'Content-Type': "application/json",
-            'X-Cisco-Meraki-API-Key': "bc960c0053036feb690c815f44f317076a0fdd0d",
+            'X-Cisco-Meraki-API-Key': APIkey,
             'User-Agent': "PostmanRuntime/7.19.0",
             'Cache-Control': "no-cache",
             'Postman-Token': "65791058-9447-4684-86da-6158024c56ee,3325d8bd-efaf-4a5b-bb32-5eb9e56ed023",
