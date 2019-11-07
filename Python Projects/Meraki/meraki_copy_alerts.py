3392440d49976db9b36c5b7e7f5b10178fdf5816
Paul <Paul@DESKTOP-1TUAUSK>
2019-11-07 10:04:44 -0800

    updated code

--- Python Projects/Meraki/meraki_copy_alerts.py
+++ Python Projects/Meraki/meraki_copy_alerts.py
@@ -43,35 +43,29 @@ networks = requests.request("GET", url, headers=headers).text
 for network_json in json.loads(networks):
     if "Azure" in network_json["name"]:
         continue
+
     print(network_json["id"])
     
-    for alerts in json.loads(alerts_main)["alerts"]:
-        #print(alerts)
-        #print(alerts["type"], alerts["enabled"], )
-        try:
-            payload = "{\n  \"alerts\": [\n    {\n      \"type\": \""+alerts["type"]+"\",\n      \"enabled\": \""+str(alerts["enabled"]).lower()+"\",\n      \"filters\": {\n        \"timeout\": 60,\n        \"selector\": \""+alerts["filters"]["selector"]+"\"\n        }\n    }\n  ]\n}"
-        except:
-            payload = "{\n  \"alerts\": [\n    {\n      \"type\": \""+alerts["type"]+"\",\n      \"enabled\": \""+str(alerts["enabled"]).lower()+"\"\n    }\n  ]\n}"
-        
-
-        time.sleep(0.2)
-        
-        url = "https://api.meraki.com/api/v0/networks/"+network_json["id"]+"/alertSettings"
-
-        headers = {
-            'Accept': "*/*",
-            'Content-Type': "application/json",
-            'X-Cisco-Meraki-API-Key': APIkey,
-            'User-Agent': "PostmanRuntime/7.19.0",
-            'Cache-Control': "no-cache",
-            'Postman-Token': "242faef5-ba9d-4281-88e7-492a724a371b,e9a0fc3a-2eb2-43d1-b5ea-a4835664268b",
-            'Accept-Encoding': "gzip, deflate",
-            'Content-Length': "95",
-            'Referer': "https://api.meraki.com/api/v0/networks/"+network_json["id"]+"/alertSettings",
-            'Connection': "keep-alive",
-            'cache-control': "no-cache"
-            }
-        
-        response = requests.request("PUT", url, data=payload, headers=headers)
+    print(alerts_main)
     
-        print(response.text)
+    time.sleep(0.2)
+   
+    url = "https://api.meraki.com/api/v0/networks/"+network_json["id"]+"/alertSettings"
+
+    headers = {
+        'Accept': "*/*",
+        'Content-Type': "application/json",
+        'X-Cisco-Meraki-API-Key': APIkey,
+        'User-Agent': "PostmanRuntime/7.19.0",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "242faef5-ba9d-4281-88e7-492a724a371b,e9a0fc3a-2eb2-43d1-b5ea-a4835664268b",
+        'Accept-Encoding': "gzip, deflate",
+        'Content-Length': "95",
+        'Referer': "https://api.meraki.com/api/v0/networks/"+network_json["id"]+"/alertSettings",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+        }
+    
+    response = requests.request("PUT", url, data=alerts_main, headers=headers)
+
+    print(response.text)
