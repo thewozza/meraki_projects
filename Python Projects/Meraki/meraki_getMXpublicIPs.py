e8d4262dc7a083c9c2c89a77845d8f795c2a2824
Paul <Paul@DESKTOP-1TUAUSK>
2020-01-29 12:38:03 -0800

    Added a CSV output option

--- Python Projects/Meraki/meraki_getMXpublicIPs.py
+++ Python Projects/Meraki/meraki_getMXpublicIPs.py
@@ -41,6 +41,22 @@ while choice == 0:
             print("You selected the organization",data["name"])
             organization = data["id"]
 
+# then we build a nice little menu so you can selection the organization you want
+readable = 11
+while readable == 11:
+    print ("Now you get to choose the output format!")
+    print("1: Human readble")
+    print("2: CSV")
+     
+    readable = int(input ("Please choose your output format: "))
+     
+    if readable == 1:
+        print("Human readable it is!")
+    else:
+        print("Okay you're getting CSV")
+        readable = 0
+    print()
+
 # now that we know what organization you want, we comb through the inventory looking for firewalls
 url = "https://api.meraki.com/api/v0/organizations/"+organization+"/inventory"
 response = requests.request("GET", url, headers=headers, data = payload).text
@@ -52,24 +68,55 @@ for org_devices in json.loads(response):
     if "MX" in org_devices["model"]:
         org_mx.append(org_devices.copy())
 
+
+
 # then we iterate through this list, and pull down the uplink info for each MX device
 for mx_list in org_mx:
+    line = []
     url = "https://api.meraki.com/api/v0/networks/"+mx_list["networkId"]+"/devices/"+mx_list["serial"]+"/uplink"
     response = requests.request("GET", url, headers=headers, data = payload).text
     
-    print(mx_list["name"])
+    if readable:
+        print(mx_list["name"])
+    else:
+        line.append(mx_list["name"])
     
     # each MX has two uplinks so we iterate through them
     for mx_uplinks in json.loads(response):
         # the other variables don't exist if the link is down, and it makes the script barf
         # I could have coded error handling for that, but it is easier to just avoid it
         if mx_uplinks["status"] == "Not connected":
-            print(mx_uplinks["interface"],"Not connected")
-        else:
-            print(mx_uplinks["interface"],mx_uplinks["status"])
-            print("IP Address",mx_uplinks["ip"],"Gateway IP",mx_uplinks["gateway"],"DNS",mx_uplinks["dns"])
-            if mx_uplinks["usingStaticIp"]:
-                print("Using Static IP")
+            if readable:
+                print(mx_uplinks["interface"],"Not connected")
             else:
-                print("Using Dynamic IP")
-    print()
+                line.append(mx_uplinks["interface"])
+                line.append("Not connected")
+        else:
+            if readable:
+                print(mx_uplinks["interface"],mx_uplinks["status"])
+            else:
+                line.append(mx_uplinks["interface"])
+                line.append(mx_uplinks["status"])
+            if readable:
+                print("IP Address",mx_uplinks["ip"],"Gateway IP",mx_uplinks["gateway"],"DNS",mx_uplinks["dns"])
+            else:
+                line.append("IP Address")
+                line.append(mx_uplinks["ip"])
+                line.append("Gateway IP")
+                line.append(mx_uplinks["gateway"])
+                line.append("DNS")
+                line.append(mx_uplinks["dns"])
+            if mx_uplinks["usingStaticIp"]:
+                if readable:
+                    print("Using Static IP")
+                else:
+                    line.append("Using Static IP")
+            else:
+                if readable:
+                    print("Using Dynamic IP")
+                else:
+                    line.append("Using Dynamic IP")
+    if readable:
+        print()
+    else:
+        print(",".join(line))
